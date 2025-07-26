540724284a7ed88442b957d79ead3f520d9e8cf8
Walter Wang <wzc314159@hotmail.com>
2025-07-26 20:53:38 +0800

    deployment

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -11,5 +11,8 @@ export const typeOrmConfig = registerAs(
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     synchronize: Boolean(process.env.DB_SYNC ?? false),
+    ssl: {
+      rejectUnauthorized: false,
+    },
   }),
 );
